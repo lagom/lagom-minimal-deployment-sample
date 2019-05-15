fe4c4935d7ca330db5aeb8ccb7978c6c2158dac5
Ignasi Marimon-Clos <ignasi@lightbend.com>
2019-05-15 10:08:00 +0200

    Automate deployment from Travis
    Review order of tar flags
    
    Add tests to ensure oc and kustomize are installed
    
    Stop deploy early if login fails
    
    Adds extra logging
    
    A bit more logging
    
    Split build into conditional stages
    
    Recreates the secret escaping the value
    
    Adds argument name
    
    Adds a check to ensure the CP2_PWD env var exists
    
    Use simpler syntax
    
    Escape pwd
    
    Echo a fake pwd to debug login issue
    
    Relocarte pwd in travis yml
    
    Review ENV_VAR existence check
    
    More echoing of pwds
    
    Rebuild secreat for travis.com (not travis.org)
    
    Add the correct pwd (and remove logs

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 export OPENSHIFT_SERVER=centralpark2.lightbend.com
 export OPENSHIFT_PROJECT=lagom-scala-openshift-smoketests
 export IMAGE_HELLO=hello-lagom
@@ -11,17 +9,28 @@ export TAG=1.7-SNAPSHOT
 export DOCKER_REGISTRY_SERVER=docker-registry-default.centralpark2.lightbend.com
 export DOCKER_REGISTRY=$DOCKER_REGISTRY_SERVER/$OPENSHIFT_PROJECT
 
-TOKEN=$1
+## 1. login to the cluster
+echo "Attempting login to Openshift cluster (this will fail on PR builds)"
+if [ -z ${CP2_PLAY_PASSWORD+x} ]; then echo "CP2_PLAY_PASSWORD is unset."; else echo "CP2_PLAY_PASSWORD is available."; fi
+oc login https://$OPENSHIFT_SERVER --username=play-team --password=$CP2_PLAY_PASSWORD  || exit 1
 
-oc login https://$OPENSHIFT_SERVER --token=$TOKEN
-
+## 2. Recreate the Openshift Project
+# 2.1 while the project is deleted, we build the docker images
 oc delete project $OPENSHIFT_PROJECT
-# while the project is deleted, we build the docker imges
+
+## 3. create the docker images
 sbt clean docker:publishLocal
-oc new-project    $OPENSHIFT_PROJECT
 
-docker login -p $TOKEN -u unused $DOCKER_REGISTRY_SERVER
+# 2.2 Do create the project (deletion often takes time to complete and propagate)
+oc new-project $OPENSHIFT_PROJECT
 
+## 4. `docker login` to the image stream of the project (aka, use the docker registry 
+##    dedicated to our openshift project)
+# `docker login` to the openshift registry only works with token (not username/password credentials)
+# so we inject the token via stdin and ignore the username argument.
+oc whoami -t | docker login -u unused --password-stdin $DOCKER_REGISTRY_SERVER
+
+## 5. Tag images and push them
 echo "Tagging image: $IMAGE_HELLO:$TAG $DOCKER_REGISTRY/$IMAGE_HELLO:$TAG"
 echo "  - $IMAGE_HELLO:$TAG"
 echo "  - $DOCKER_REGISTRY/$IMAGE_HELLO:$TAG"
@@ -35,7 +44,10 @@ docker tag $IMAGE_HELLO_PROXY:$TAG $DOCKER_REGISTRY/$IMAGE_HELLO_PROXY:$TAG
 docker push $DOCKER_REGISTRY/$IMAGE_HELLO:$TAG
 docker push $DOCKER_REGISTRY/$IMAGE_HELLO_PROXY:$TAG
 
+## 6. Deploy
 kustomize build deployment/overlays/openshift | oc apply -f -
 
 echo "Try this deployment using:"
+echo "export OPENSHIFT_SERVER=centralpark2.lightbend.com"
+echo "export OPENSHIFT_PROJECT=lagom-scala-openshift-smoketests"
 echo 'curl -H "Host: my-lagom-openshift-smoketests.example.org"  https://$OPENSHIFT_PROJECT.$OPENSHIFT_SERVER/proxy/rest-hello/alice'
